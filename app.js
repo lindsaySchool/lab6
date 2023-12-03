0a8b0313cf036e4d9020b2f0bbecf45c5157e6aa
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-03 16:43:18 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+//require cors
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +20,9 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//use cors
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
