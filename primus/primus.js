6647c60195d1b71be3b0b42b0af5901682d881ac
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-05 15:45:21 +0100

    feat: add primus server

--- primus/primus.js
+++ primus/primus.js
@@ -1,9 +1,28 @@
-const Primus = require('primus');
+const Primus = require("primus")
 
+const go = (server) => {
+    let primus = new Primus(server, {/* options */});
 
-let go = (Server) => {
-    new Primus(server, {/* options */})
+    primus.on('connection', (spark) => {
+        console.log("received spark");
+
+        spark.on('data', (data)=>{
+            try{
+                let parsedData = JSON.parse(data);
+                console.log('received data from client: ', parsedData.data.scores);
+                //console.log(data);
+                primus.write({
+                    action: "update",
+                    data: {
+                        scores: parsedData.data.scores
+                    }
+                }); 
+            } catch (error){
+                console.log("De error is" +error);
+            }
+           
+        })
+    });
 }
 
-// export
 module.exports.go = go;
